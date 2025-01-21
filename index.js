5669dd84960dd55a096e52d57a0551da9104d38d
aravind1100 <aravindavd1999@gmail.com>
2025-01-22 00:44:00 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import { format } from "date-fns";
+
+const app = express();
+app.use(cors());   //Which domain,port
+const PORT = 4000; 
+const DIRECTORY = "TimeStamp";
+
+//API generation to write file
+
+app.get("/writefile", (req, res) => {
+  try {
+    let today = format(new Date(), "dd-MM-yyyy-HH-mm-ss");
+
+    const filePath = `${DIRECTORY}/${today}`;
+    fs.writeFileSync(filePath, `${today}`, "utf8");
+
+    res.status(201).send({ message: "File Created", filename: `${today}.txt` });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Unable to create file" });
+  }
+});
+
+//API generation to read all files
+
+app.get("/readfiles", (req, res) => {
+  try {
+    const files = fs.readdirSync(DIRECTORY);
+    const fileContents = files.map((file) => {
+      const data = fs.readFileSync(`${DIRECTORY}/${file}`, "utf8");
+      return { filename: file, fileContent: data };
+    });
+
+    res.status(200).send(fileContents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Unable to read files" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("App is listening on Port :", PORT);
+});
